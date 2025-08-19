da5580c1b4b5fd76f855e2957d74e1669b37cd76
cojay80 <cojay80@naver.com>
2025-08-19 19:30:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// server.js (Express 정적 서버; HTML 넣지 마세요)
+// server.js
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 레포 루트 정적 서빙: /, /pages, /js, /css, /images ...
+// 레포 루트 정적 서빙
 app.use(express.static(path.join(__dirname)));
 
 // 홈
@@ -13,11 +13,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// 헬스체크(선택)
+// 헬스체크(Health Check Path에 쓸 엔드포인트)
 app.get('/healthz', (_req, res) => res.send('ok'));
-
-// 멀티페이지 구조라 SPA catch-all 불필요
-// app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(PORT, () => {
   console.log(`soi-homestudy running on port ${PORT}`);
